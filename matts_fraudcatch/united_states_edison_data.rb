8005a156cf8b9bdbc08bf00e7a809d676bde2ed9
Matthew Biesen <mbiesen@clm-mbiesen.local>
2020-11-24 03:56:31 -0600

    changes

--- matts_fraudcatch/united_states_edison_data.rb
+++ matts_fraudcatch/united_states_edison_data.rb
@@ -42,18 +42,18 @@ class UnitedStatesEdisonData
 
   def print_drop_totals_for_states
     @all_states_edison_data.each do |state|
-      puts "State: #{state.state_name}, TOTAL DROP: #{state.total_vote_count_drop}, Biden dropped more? #{state.biden_drop_more_than_trump?}"
+      state.print_times_where_total_vote_count_dropped
     end
   end
 
-  def print_sorted_drop_totals_for_trump
+  def print_sorted_state_drop_totals_for_trump
     sorted = @all_states_edison_data.sort_by {|state| state.trump_total_drop}
     sorted.each do |state|
       puts "State: #{state.state_name}, Trump total drop: #{state.trump_total_drop}"
     end
   end
 
-  def print_sorted_drop_totals_for_biden
+  def print_sorted_state_drop_totals_for_biden
     sorted = @all_states_edison_data.sort_by {|state| state.biden_total_drop}
     sorted.each do |state|
       puts "State: #{state.state_name}, Biden total drop: #{state.biden_total_drop}"
@@ -63,8 +63,7 @@ class UnitedStatesEdisonData
   def print_most_common_day_for_vote_drops_in_state
     @all_states_edison_data.each do |state|
       date_count = Hash.new(0)
-      tsdata = state.vote_drop_timeseries_data
-      dates = tsdata.map{|data| data['timestamp'].to_date}
+      dates = state.times_total_vote_count_dropped.map{|x| x['current_data']['timestamp'].to_date}
       dates.each {|date| date_count[date] += 1}
       most_common_date = date_count.sort_by { |date,number| number}.last[0]
       puts "State #{state.state_name} most commonly dropped votes on #{most_common_date}"
@@ -73,16 +72,16 @@ class UnitedStatesEdisonData
 
   def print_states_where_candiate_dropped_more(candidate)
     if candidate.downcase == 'biden'
-      @all_states_edison_data.select{|state| state.biden_drop_more_than_trump?}.each{|state| puts state.state_name}
+      @all_states_edison_data.select{|state| state.biden_total_drop < state.trump_total_drop}.each{|state| puts state.state_name}
     else
-      @all_states_edison_data.select{|state| !state.biden_drop_more_than_trump?}.each{|state| puts state.state_name}
+      @all_states_edison_data.select{|state| state.biden_total_drop > state.trump_total_drop}.each{|state| puts state.state_name}
     end
   end
 
   def print_states_by_vote_drop_frequency
-    sorted = @all_states_edison_data.sort_by {|state| state.vote_drop_timeseries_data.count}
+    sorted = @all_states_edison_data.sort_by {|state| state.times_total_vote_count_dropped.count}
     sorted.each do |state|
-      puts "State: #{state.state_name} Dropped Votes #{state.vote_drop_timeseries_data.count} times"
+      puts "State: #{state.state_name} Dropped Votes #{state.times_total_vote_count_dropped.count} times"
     end
   end
 
@@ -92,7 +91,7 @@ class UnitedStatesEdisonData
 
   def print_winner_and_if_winner_dropped_more
     @all_states_edison_data.each do |state| 
-      did_winner_drop_more = state.winner == 'biden' ? state.biden_drop_more_than_trump? : !state.biden_drop_more_than_trump?
+      did_winner_drop_more = state.winner == 'biden' ? state.biden_total_drop < state.trump_total_drop : state.biden_total_drop > state.trump_total_drop
       puts "#{state.state_name}: #{state.winner}: winner dropped more? #{did_winner_drop_more}"
     end
   end
@@ -102,7 +101,7 @@ class UnitedStatesEdisonData
     @all_states_edison_data.each do |state|
       next unless state.winner == 'trump'
 
-      did_winner_drop_more = state.winner == 'biden' ? state.biden_drop_more_than_trump? : !state.biden_drop_more_than_trump?
+      did_winner_drop_more = state.biden_total_drop > state.trump_total_drop
       if did_winner_drop_more
         puts state.state_name
       end
@@ -114,7 +113,7 @@ class UnitedStatesEdisonData
     @all_states_edison_data.each do |state|
       next unless state.winner == 'biden'
 
-      did_winner_drop_more = state.winner == 'biden' ? state.biden_drop_more_than_trump? : !state.biden_drop_more_than_trump?
+      did_winner_drop_more = state.winner == 'biden' ? state.biden_total_drop < state.trump_total_drop
       if did_winner_drop_more
         puts state.state_name
       end
